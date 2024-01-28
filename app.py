ad2a28f04bed3611c7a3c0b7152aab8cfce46916
Mwelwa <mwelwankuta@gmail.com>
2024-01-28 09:54:57 +0200

    chore: host as localhost

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ port = int(os.environ.get('PORT', 8080))
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=port)
+    serve(app, host="127.0.0.1", port=port)
